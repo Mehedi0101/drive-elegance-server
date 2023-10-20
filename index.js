d335477f3c10366113e7d86c64fbc35b2cb4b510
Mehedi0101 <mehedi29121998@gmail.com>
2023-10-20 21:45:36 +0600

    deployment issues fixed

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
-
 const app = express();
+const cors = require("cors");
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
+
+
+
 
 app.use(cors());
 app.use(express.json());
@@ -21,8 +23,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
-
         // products-collection
         const productCollection = client.db("productDB").collection("product");
         const userCollection = client.db("userDB").collection("user");
@@ -162,9 +162,6 @@ async function run() {
             )
         })
 
-
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
